ae4d6a67f1ea6093698f5bd588b10d8d1dd4e949
Laura Urrea <Laura@Lauras-MacBook-Air.local>
2019-08-29 21:58:26 -0400

    late night coding

--- src/components/Navigation/MainNavigation.js
+++ src/components/Navigation/MainNavigation.js
@@ -7,7 +7,7 @@ const mainNavigation = props => (
 
     <header className="main-navigation">
         <div className="main-navigation__logo">
-            <h1>Imigro</h1>
+            <h1>Real Estate Inventory List</h1>
         </div>
         <nav className="main-navigation__item">
         <ul>
@@ -15,7 +15,7 @@ const mainNavigation = props => (
                 <NavLink to="/auth">Authenticate</NavLink>
             </li>
             <li>
-                <NavLink to="/events">Events</NavLink>
+                <NavLink to="/events">Properties</NavLink>
             </li>
             <li>
             <NavLink to="/bookings">Bookings</NavLink>
