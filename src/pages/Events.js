f3dbc10b4455ccd4ca3fb959dc535d4192347ffe
Laura Urrea <Laura@Lauras-MacBook-Air.local>
2019-12-15 12:43:01 -0500

    noon

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+
+import Modal from '../components/Modal/Modal';
 import './Events.css';
 
 class Eventspage extends Component {
     render () {
         return (
+            <React.Fragment>
+                <Modal title="Add Event" canCancel canConfirm>
+                    <p>Modal Content</p>
+                </Modal>
             <div className="events-control">
-                <button className="btn">Create Event</button>
+                <p>Book Property Listing Showings</p>
+                    <button className="btn">Create Event</button>
             </div>
+            </React.Fragment>
         );
     }
 }
