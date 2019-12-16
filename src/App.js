38105f12e8ccb951a0710fcd758bfc4ed8c3f249
Laura Urrea <Laura@Lauras-MacBook-Air.local>
2019-12-16 09:20:37 -0500

    noon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
 import MainNavigation from './components/Navigation/MainNavigation';
-import AuthContext from './context/Auth-context';
+import AuthContext from './components/context/Auth-context';
 
 class App extends Component {
 
