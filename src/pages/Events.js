4b7d31b2cc0abf50b6a6a30f33edf5bc0947c2e8
Laura Urrea <Laura@Lauras-MacBook-Air.local>
2019-12-16 07:55:48 -0500

    noon

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -23,11 +23,11 @@ class EventsPage extends Component {
     }
 
     startCreateEventHandler = () => {
-        this.setState({creating: true });
+        this.setState({ creating: true });
     };
 
     modalConfirmHandler = () => {
-        this.setState({creating: false });
+        this.setState({ creating: false });
         const title = this.titleElRef.current.value;
         const price = this.priceElRef.current.value;
         const date = this.dateElRef.current.value;
@@ -57,43 +57,52 @@ if  (
             <React.Fragment>
             
              
-                  <Modal 
+                  {this.state.creating && (
+                    <Modal 
                     title="Add Event" 
                     canCancel 
                     canConfirm 
                     on Cancel={this.modalCancelHandler} 
-                    on Confirm={this.modalConfirmHandler}> 
-                  <p>Modal Content to be written</p>
-                  </Modal>
-
-                <div className="events-control">
-                <form>
+                    on Confirm={this.modalConfirmHandler}
+                    > 
+                  <form>
 
                 <div classname="form-control">
                 <label htmlFor="title">Title</label>
-                <input type="text" id="title" ref={this.titleElRef}></input>
+                <input type="text" id="title" ref={this.titleElRef} />
                 </div>
 
                 <div classname="form-control">
                 <label htmlFor="price">Price</label>
-                <input type="number" id="price" ref={this.priceElRef}></input>
+                <input type="number" id="price" ref={this.priceElRef} />
                 </div>
 
                 <div classname="form-control">
                 <label htmlFor="date">Date</label>
-                <input type="date" id="date" ref={this.dateElRef}></input>
+                <input type="date" id="date" ref={this.dateElRef} />
                 </div>
 
                 <div classname="form-control">
-                <label htmlFor="description">Description</label>
-                <textarea id="description" rows="4" ref={this.descriptionElRef}></textarea>
+                <label htmlFor="description">Notes</label>
+                <textarea id="description" 
+                rows="4" 
+                ref={this.descriptionElRef}
+                />
                 </div>
-
-                
                 </form>
+                </Modal>
+                )}
 
-                    <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
-                </div>
+                {this.context.token && (
+                    <div className="events-control">
+                    <p>Create Event Showings</p>
+                        <button className="btn" onClick={this.startCreateEventHandler}>
+                        Create Event
+                        </button>
+                    </div>
+                )}
+                
+                
             </React.Fragment>
         );
     }
